8ec12077b174cbc742ab9863b02d5b74f0d3ea80
Oleksandr Marchenko <alex@marchenko.co.uk>
2025-01-07 10:55:59 +0000

    Add Authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_134009) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_06_152235) do
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "desc"
---
+++ spec/views/authors/index.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "authors/index", type: :view do
+  before(:each) do
+    assign(:authors, [
+      Author.create!(),
+      Author.create!()
+    ])
+  end
+
+  it "renders a list of authors" do
+    render
+    cell_selector = 'div>p'
+  end
+end
