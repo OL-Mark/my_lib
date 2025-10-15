f7d7556fc23f6bc0baf0b664ef264d93b871e8af
Oleksandr Marchenko <oleks@marchenko.co.uk>
2025-10-15 11:35:47 +0100

    Comment out less important validations at this stage

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,20 +9,17 @@ class Book < ApplicationRecord
   belongs_to :publisher
 
   validates :title, presence: true
-  validates :authors, presence: true
-  validates :publisher_id, presence: true
+  # I decided to disable these validations so far to start populating the database with books faster
+  # validates :authors, presence: true
+  # validates :publisher_id, presence: true
   validates :isbn, allow_nil: true, uniqueness: true, format: {
     with: VALID_ISBN_REGEX,
     message: "must be a valid 10 or 13-digit ISBN (e.g., 123456789X or 9781234567897)"
   }
   validate :isbn_checksum
 
-  # TODO: Add full_title method implementation which should contain author's name
-  def full_title; end
-
   private
 
-  # Custom validation for ISBN checksum
   def isbn_checksum
     return if isbn.blank?
 
@@ -35,14 +32,12 @@ class Book < ApplicationRecord
     end
   end
 
-  # Validate ISBN-10 checksum
   def valid_isbn_10?(isbn)
     sum = isbn.chars[0..8].each_with_index.sum { |digit, index| digit.to_i * (10 - index) }
     checksum = isbn[-1] == "X" ? 10 : isbn[-1].to_i
     (sum + checksum) % 11 == 0
   end
 
-  # Validate ISBN-13 checksum
   def valid_isbn_13?(isbn)
     sum = isbn.chars[0..11].each_with_index.sum do |digit, index|
       digit.to_i * (index.even? ? 1 : 3)
